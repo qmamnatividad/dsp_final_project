bdca4878a99a87768471352a04bb7bebcbd1d8ea
Marc Natividad <133554826+qmamnatividad@users.noreply.github.com>
2023-11-25 16:56:40 +0800

    Update dsp_final_project.py

--- dsp_final_project.py
+++ dsp_final_project.py
@@ -1,39 +1,30 @@
 import streamlit as st
+import numpy as np
 from skimage import io, img_as_float
-from skimage.filters import unsharp_mask
-from skimage.filters import gaussian
-import matplotlib.pyplot as plt
+from skimage.filters import unsharp_mask, gaussian
 
-# Streamlit app
-st.title("Unsharp Masking Demo")
+def main():
+    st.title("Unsharp Masking with Streamlit")
 
-# Image uploader
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Upload image through Streamlit
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-if uploaded_file is not None:
-    # Read the image
-    img = img_as_float(io.imread(uploaded_file))
+    if uploaded_file is not None:
+        # Read the image
+        img = img_as_float(io.imread(uploaded_file))
 
-    # Gaussian blur
-    gaussian_img = gaussian(img, sigma=2, mode='constant', cval=0.0)
+        # Apply Gaussian blur
+        gaussian_img = gaussian(img, sigma=2, mode='constant', cval=0.0)
 
-    # Unsharp mask
-    img2 = (img - gaussian_img) * 2.
+        # Calculate unsharp mask
+        img2 = (img - gaussian_img) * 2.
 
-    # Sharpened image
-    img3 = img + img2
+        # Combine original and unsharp mask
+        img3 = img + img2
 
-    # Display original image
-    st.subheader("Original Image")
-    st.image(img, caption='Original Image', use_column_width=True)
+        # Display the images
+        st.image([img, gaussian_img, img3], caption=['Original', 'Blurred', 'Original - Blurred'], 
+                 width=300, use_column_width=True)
 
-    # Display blurred image
-    st.subheader("Blurred Image")
-    st.image(gaussian_img, caption='Blurred Image', use_column_width=True)
-
-    # Display sharpened image
-    st.subheader("Sharpened Image (Original - Blurred)")
-    st.image(img3, caption='Original - Blurred', use_column_width=True)
-
-    # Display matplotlib plot (optional)
-    st.pyplot(plt)
+if __name__ == "__main__":
+    main()
