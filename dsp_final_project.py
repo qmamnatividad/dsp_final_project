5f10c9c9750621e707f77eda8adeda1b827fff2d
Marc Natividad <133554826+qmamnatividad@users.noreply.github.com>
2023-11-27 02:23:53 +0800

    Update dsp_final_project.py

--- dsp_final_project.py
+++ dsp_final_project.py
@@ -10,26 +10,30 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
     if uploaded_file is not None:
-        # Read the image
-        img = img_as_float(io.imread(uploaded_file))
+        try:
+            # Read the image
+            img = img_as_float(io.imread(uploaded_file))
 
-        # Apply Gaussian blur
-        gaussian_img = gaussian(img, sigma=2, mode='constant', cval=0.0)
+            # Apply Gaussian blur
+            gaussian_img = gaussian(img, sigma=2, mode='constant', cval=0.0)
 
-        # Calculate unsharp mask
-        img2 = (img - gaussian_img) * 2.
+            # Calculate unsharp mask
+            img2 = (img - gaussian_img) * 2.
 
-        # Combine original and unsharp mask
-        img3 = img + img2
+            # Combine original and unsharp mask
+            img3 = img + img2
 
-        # Clip the image values to be within [0.0, 1.0]
-        img = np.clip(img, 0.0, 1.0)
-        gaussian_img = np.clip(gaussian_img, 0.0, 1.0)
-        img3 = np.clip(img3, 0.0, 1.0)
+            # Clip the image values to be within [0.0, 1.0]
+            img = np.clip(img, 0.0, 1.0)
+            gaussian_img = np.clip(gaussian_img, 0.0, 1.0)
+            img3 = np.clip(img3, 0.0, 1.0)
 
-        # Display the images with a specified color map (you can adjust 'cmap' as needed)
-        st.image([img, gaussian_img, img3], caption=['Original', 'Blurred', 'Original - Blurred'],
-                 width=300, use_column_width=True, cmap='viridis')
+            # Display the images with a specified color map (you can adjust 'cmap' as needed)
+            st.image([img, gaussian_img, img3], caption=['Original', 'Blurred', 'Original - Blurred'],
+                     width=300, use_column_width=True, cmap='viridis')
+        except Exception as e:
+            # Print the exception for debugging
+            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
